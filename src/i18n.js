2fb2a92138551d6ecc2ced4ee19373bd26aec33a
DhiaLajili <dhiaelajili4@gmail.com>
2022-08-25 14:36:17 +0100

    deployed app

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,10 @@
-import i18n from "i18next";
-import Backend from "i18next-xhr-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-import { initReactI18next } from "react-i18next";
+import i18n from "i18next"
+import Backend from "i18next-xhr-backend"
+import LanguageDetector from "i18next-browser-languagedetector"
+import { initReactI18next } from "react-i18next"
 
-const fallbackLng = ["en"];
-const availableLanguages = ["en", "fr"];
+const fallbackLng = ["en"]
+const availableLanguages = ["en", "fr"]
 
 const options = {
   // order and from where user language should be detected
@@ -30,15 +30,17 @@ const options = {
 
   // only detect languages that are in the whitelist
   checkWhitelist: true,
-};
+}
 
 i18n
-  .use(Backend) // load translation using xhr -> see /public/locales. We will add locales in the next step
 
   .use(LanguageDetector) // detect user language
 
   .use(initReactI18next) // pass the i18n instance to react-i18next.
   .init({
+    backend: {
+      loadPath: "locales/{{lng}}/{{ns}}.json",
+    },
     fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
     debug: true,
     whitelist: availableLanguages,
@@ -47,6 +49,6 @@ i18n
     interpolation: {
       escapeValue: false,
     },
-  });
+  })
 
-export default i18n;
+export default i18n
